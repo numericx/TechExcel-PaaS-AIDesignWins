122061a9c1e7ae7d335886ee17348fc5b73888f2
Steeve Brechmann <steevebrechmann@gmail.com>
2025-04-08 11:41:38 -0400

    Update script with managed identity

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,3 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
-
+CREATE USER "id-6xx2ym6ca4f3m" FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER "id-6xx2ym6ca4f3m";
+ALTER ROLE db_datawriter ADD MEMBER "id-6xx2ym6ca4f3m";
