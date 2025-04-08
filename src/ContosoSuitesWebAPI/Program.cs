cf4b596e42134fa4cfa86675cf0c0b866cc797a2
Steeve Brechmann <steevebrechmann@gmail.com>
2025-04-08 16:42:01 -0400

    Updated WebAPI

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddSingleton<IVectorizationService, VectorizationService>();
 builder.Services.AddSingleton<MaintenanceCopilot, MaintenanceCopilot>();
 
 // Create a single instance of the DatabaseService to be shared across the application.
-builder.Services.AddSingleton<IDatabaseService, DatabaseService>((_) => 
+builder.Services.AddSingleton<IDatabaseService, DatabaseService>((_) =>
 {
     var connectionString = builder.Configuration.GetConnectionString("ContosoSuites");
     return new DatabaseService(connectionString!);
@@ -68,7 +68,7 @@ app.UseHttpsRedirection();
 
 /**** Endpoints ****/
 // This endpoint serves as the default landing page for the API.
-app.MapGet("/", async () => 
+app.MapGet("/", async () =>
 {
     return "Welcome to the Contoso Suites Web API!";
 })
@@ -76,7 +76,7 @@ app.MapGet("/", async () =>
     .WithOpenApi();
 
 // Retrieve the set of hotels from the database.
-app.MapGet("/Hotels", async () => 
+app.MapGet("/Hotels", async () =>
 {
     // throw new NotImplementedException();
     var hotels = await app.Services.GetRequiredService<IDatabaseService>().GetHotels();
@@ -86,7 +86,7 @@ app.MapGet("/Hotels", async () =>
     .WithOpenApi();
 
 // Retrieve the bookings for a specific hotel.
-app.MapGet("/Hotels/{hotelId}/Bookings/", async (int hotelId) => 
+app.MapGet("/Hotels/{hotelId}/Bookings/", async (int hotelId) =>
 {
     // throw new NotImplementedException();
     var bookings = await app.Services.GetRequiredService<IDatabaseService>().GetBookingsForHotel(hotelId);
@@ -96,7 +96,7 @@ app.MapGet("/Hotels/{hotelId}/Bookings/", async (int hotelId) =>
     .WithOpenApi();
 
 // Retrieve the bookings for a specific hotel that are after a specified date.
-app.MapGet("/Hotels/{hotelId}/Bookings/{min_date}", async (int hotelId, DateTime min_date) => 
+app.MapGet("/Hotels/{hotelId}/Bookings/{min_date}", async (int hotelId, DateTime min_date) =>
 {
     // throw new NotImplementedException();
     var bookings = await app.Services.GetRequiredService<IDatabaseService>().GetBookingsByHotelAndMinimumDate(hotelId, min_date);
@@ -109,7 +109,7 @@ app.MapGet("/Hotels/{hotelId}/Bookings/{min_date}", async (int hotelId, DateTime
 app.MapPost("/Chat", async Task<string> (HttpRequest request) =>
 {
     var message = await Task.FromResult(request.Form["message"]);
-    
+
     return "This endpoint is not yet available.";
 })
     .WithName("Chat")
@@ -129,13 +129,15 @@ app.MapGet("/Vectorize", async (string text, [FromServices] IVectorizationServic
 app.MapPost("/VectorSearch", async ([FromBody] float[] queryVector, [FromServices] IVectorizationService vectorizationService, int max_results = 0, double minimum_similarity_score = 0.8) =>
 {
     // Exercise 3 Task 3 TODO #3: Insert code to call the ExecuteVectorSearch function on the Vectorization Service. Don't forget to remove the NotImplementedException.
-    throw new NotImplementedException();
+    // throw new NotImplementedException();
+    var results = await vectorizationService.ExecuteVectorSearch(queryVector, max_results, minimum_similarity_score);
+    return results;
 })
     .WithName("VectorSearch")
     .WithOpenApi();
 
 // This endpoint is used to send a message to the Maintenance Copilot.
-app.MapPost("/MaintenanceCopilotChat", async ([FromBody]string message, [FromServices] MaintenanceCopilot copilot) =>
+app.MapPost("/MaintenanceCopilotChat", async ([FromBody] string message, [FromServices] MaintenanceCopilot copilot) =>
 {
     // Exercise 5 Task 2 TODO #10: Insert code to call the Chat function on the MaintenanceCopilot. Don't forget to remove the NotImplementedException.
     throw new NotImplementedException();
